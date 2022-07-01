74dec89eca5ed626bb3f36440b34b587c7069dfe
saranell <76405224+saranell@users.noreply.github.com>
2022-07-01 17:19:44 -0500

    JS updated for up, down, left, right buttons

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
 window.addEventListener("load", () => {
-  const takeOff = document.getElementById("takeoff");
   const status = document.getElementById("flightStatus");
   const background = document.getElementById("shuttleBackground");
   const height = document.getElementById("spaceShuttleHeight");
-  const land = document.getElementById("landing");
-  const down = document.getElementById("down");
+  // let shuttleWidth = document.getElementById('spaceShuttleWidth');
   const img = document.getElementById("rocket");
 
+  const takeOff = document.getElementById("takeoff");
   takeOff.addEventListener("click", () => {
     result = window.confirm("Confirm the shuttle is ready for takeoff.");
     if (result) {
@@ -16,6 +15,7 @@ window.addEventListener("load", () => {
     }
   });
 
+  const land = document.getElementById("landing");
   landing.addEventListener("click", () => {
     response = window.alert("The shuttle is landing. Landing gear engaged.");
     if (response) {
@@ -25,6 +25,32 @@ window.addEventListener("load", () => {
     }
   });
 
+  const missionAbort = this.document.getElementById("missionAbort");
+  missionAbort.addEventListener("click", function () {
+    result = window.confirm("Are you sure you want to end the mission?");
+    if (result) {
+      background.style.backgroundColor = "green";
+      height.innerHTML = "0";
+      status.innerHTML = "Mission aborted";
+    }
+  });
+
+  let shuttleWidth = document.getElementById("spaceShuttleWidth");
+
+  let right = document.getElementById("right");
+  right.addEventListener("click", function () {
+    movement = parseInt(img.style.left) + 10 + "px";
+    img.style.left = movement;
+    shuttleWidth.innerHTML = parseInt(shuttleWidth.innerHTML) + 10000;
+  });
+
+  let left = document.getElementById("left");
+  left.addEventListener("click", function () {
+    movement = parseInt(img.style.left) - 10 + "px";
+    img.style.left = movement;
+  });
+
+  const down = document.getElementById("down");
   down.addEventListener("click", () => {
     movement = parseInt(img.style.bottom) - 10 + "px";
     img.style.bottom = movement;
